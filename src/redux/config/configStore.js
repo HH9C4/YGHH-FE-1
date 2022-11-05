bb6faca91811dfddb3ac1a0ff39f0fd3942d8e9b
joyfive <joy_five@kakao.com>
2022-11-05 17:30:38 +0900

    new branch

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,14 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
-import membersSlice from "../modules/membersSlice";
-import contentsSlice from "../modules/contentsSlice";
+import { configureStore } from "@reduxjs/toolkit"
+import membersSlice from "../modules/membersSlice"
+import contentsSlice from "../modules/contentsSlice"
 
 const store = configureStore({
-    reducer: {
-        membersSlice,
-        contentsSlice,
-    },
-    //배포 모드일때 리덕스 데브툴 사용 안함
-    devTools: process.env.REACT_APP_MOD !== 'production'
-});
+  reducer: {
+    membersSlice,
+    contentsSlice,
+  },
+  //배포 모드일때 리덕스 데브툴 사용 안함
+  devTools: process.env.REACT_APP_MOD !== "production",
+})
 
-export default store;
+export default store
+//
