a8401e58711cdc17eb673176325c3bc94e732d1b
joyfive <joy_five@kakao.com>
2022-11-11 23:44:17 +0900

    mypage components first setting

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react"
+import Alarm from "../components/mypage/Alarm"
+import MyLikes from "../components/mypage/MyLikes"
+import MyPosts from "../components/mypage/Mypost"
 
 const Mypage = () => {
-    return (
-        <div>Mypage</div>
-    )
+  return (
+    <>
+      <div>Mypage</div>
+
+      <Alarm />
+      <MyPosts />
+      <MyLikes />
+    </>
+  )
 }
 
 export default Mypage
