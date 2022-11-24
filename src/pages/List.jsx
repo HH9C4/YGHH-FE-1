f7dc06faa384c79dcf022d879233aa847a12fc1f
joyfive <joy_five@kakao.com>
2022-11-24 23:19:18 +0900

    info 1차 publishing

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect, useCallback } from "react"
+import React, { useRef, useState, useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { useNavigate, useParams } from "react-router-dom"
 import Post from "../components/features/Post"
@@ -8,7 +8,7 @@ import {
   __deactivateBookmark,
 } from "../redux/modules/contentsSlice"
 import SelectGu from "../components/features/SelectGu"
-import { useInView } from 'react-intersection-observer'
+
 import Layout from "../components/layout/Layout"
 import EditBtn from "../components/elements/EditBtn"
 
@@ -25,46 +25,15 @@ const List = () => {
   const onSelect = () => {
     setSelect(!select)
   }
-
-  const [page, setPage] = useState(0) //페이지수
-  // const [size, setSize] = useState([]) //리스트수
-  const [loading, setLoading] = useState(false)
-  // console.log("page",page)
-  const [ref, inView] = useInView()
-
-  /**  서버에서 아이템을 가지고 오는 함수 */
-  const getItems = useCallback(async () => { //의존하는 값(deps)들이 바뀌지 않는 한 기존 함수를 재사용할 수 있습니다.
-    dispatch(__getContent(obj));
-  }, [page, params, likeId, isLiked])
-
-  //기존 유즈이펙트
-  useEffect(() => {
-    getItems()
-  }, [getItems])
-
-  useEffect(() => {
-    // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
-    if (inView && !loading) {
-      setPage(prevState => prevState + 1)
-
-      // console.log("페이지",page)
-    }
-  }, [inView, loading])
-
-  useEffect(() => {
-    setPage(0)
-  }
-    , [params])
-
-  console.log("콘텐츠", contents)
-  console.log("select", select)
   let obj = {
-    page: page,
     gu: params.gu,
     sort: params.sort,
     category: params.category,
   }
 
+  useEffect(() => {
+    dispatch(__getContent(obj))
+  }, [params, likeId, isLiked])
 
   //북마크 활성화 함수
   const bookMarkOn = () => {
@@ -295,12 +264,10 @@ const List = () => {
         </div>
         <div className="mx-[26px] mt-4">
           <Post posts={contents} />
-          <div ref={ref}></div>
         </div>
         <EditBtn />
       </Layout>
     </div>
-
   )
 }
 
