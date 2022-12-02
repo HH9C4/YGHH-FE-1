7f78870ead07ae2a029c299645675c7398b664dc
임효진 <imhyojin@imhyojicBookPro.Davolink>
2022-12-02 18:37:45 +0900

    ADD : Home 순위 퍼블리싱 및 로직 변경

--- src/components/Login/KakaoLogin.js
+++ src/components/Login/KakaoLogin.js
@@ -1,7 +1,5 @@
 import React from "react"
-import kakaoLogin from "../../assets/img/kakaoLogin.png"
 import { KAKAO_AUTH_URL } from "../../api/loginKeys"
-import kakao_login_large_narrow from "../../assets/img/kakao_login_large_narrow.png"
 import kakaoLogo from "../../assets/img/kakaoLogo.svg"
 const KakaoLogin = () => {
   const handleLogin = () => {
