ea1c32176689da04049e559b7e68c731eac9ab44
joyfive <joy_five@kakao.com>
2022-12-08 22:58:45 +0900

    redux sotre conflict solve

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,15 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
 import contents from "../modules/contentsSlice"
-<<<<<<< HEAD
-=======
-import search from "../modules/searchSlice"
 import my from "../modules/mySlice"
 import members from "../modules/memberSlice"
-import chatting from "../modules/chatSlice";
->>>>>>> af41cb44e7068388251d070d80c4d1574bdbcc5b
-
-import members from "../modules/memberSlice"
-import chatting from "../modules/chatSlice";
+import chatting from "../modules/chatSlice"
 
 const store = configureStore({
   reducer: {
